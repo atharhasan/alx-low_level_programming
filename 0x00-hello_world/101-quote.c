610486b61a164150255ad4da33098bc5de070b48
Athar Hasan <athar_h12@yahoo.com>
2023-06-15 15:10:50 -0700

    hello world task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print error to the standard
+ *
+ * Return: 1 (Success)
+*/
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
