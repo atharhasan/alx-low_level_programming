5060061178e0a1f319335f434facea92b2bbc17f
Athar Hasan <athar_h12@yahoo.com>
2023-06-16 17:58:30 -0700

    if else task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: programe that print alphabet in lowercase
+ *
+ * Return:0 success
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char a = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	while (a <= 'Z')
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
