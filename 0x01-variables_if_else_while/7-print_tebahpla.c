e202674e7dc6364fdbbfd49590a62117cf5fc120
Athar Hasan <athar_h12@yahoo.com>
2023-06-16 18:46:35 -0700

    if else task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: programe that print alphabet in lowercase
+ *
+ * Return:0 success
+*/
+
+int main(void)
+{
+	char i = 'z';
+
+	while (i >= 'a')
+	{
+		putchar(i);
+		ch--;
+	}
+	putchar('\n');
+
+	return (0);
+}
