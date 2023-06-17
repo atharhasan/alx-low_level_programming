319a5c7bd8a39ca9fd9b012ddfe653be906246e3
Athar Hasan <athar_h12@yahoo.com>
2023-06-16 18:26:02 -0700

    if else task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: programe that print numbers
+ *
+ * Return:0 success
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
