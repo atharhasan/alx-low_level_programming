02680113ec602af150688607aa7bd1f17ebd9bf5
Athar Hasan <athar_h12@yahoo.com>
2023-06-16 19:04:18 -0700

    if else task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: programe that print numbers
+ *
+ * Return:0 success
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		putchar('\, ');
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
