b5e8566a149a30472e095aa5ab4494da109e33c2
Athar Hasan <athar_h12@yahoo.com>
2023-06-16 17:45:15 -0700

    if else task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: programe that print alphabet in lowercase
+ *
+ * Return:0 success
+*/
+
+int main(void)
+{
+	for (int i = 'A'; i <= 'Z'; i++)
+	{
+		char lower = tolower(x);
+
+		putcher(lower);
+	}
+	return (0);
+}
