9e9df30c2a9906047ced08bbbb1a6cbcce2f8982
Athar Hasan <athar_h12@yahoo.com>
2023-06-20 00:02:27 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+include "main.h"
+
+/**
+ * print_alphabet_x10 - funcion
+ *
+ * Description: function can print alphabet in lowercase 10 times
+*/
+
+void print_alphabet_x10(void)
+{
+	int i = 'a';
+
+	for (int j = 0; j <= 10; j++)
+	{
+		while (i <= 'z')
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+	}
+}
