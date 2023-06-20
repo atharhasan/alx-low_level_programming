756cf0dc8c44c4f96fc027df72b741f9bdfbcc42
Athar Hasan <athar_h12@yahoo.com>
2023-06-20 11:22:34 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - function
+ *
+ * Description: function that computes the absolute value of an integer.
+ *
+ * @r: parameter is char that want to check
+ *
+ * Return: 1 success
+*/
+
+int _abs(int r)
+{
+	int n = abs(r);
+
+	_putchar(n);
+}
