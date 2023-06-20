7d2190c19aeeddf170ce575f9547c6e8777065b7
Athar Hasan <athar_h12@yahoo.com>
2023-06-19 23:06:35 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: c programe that can print putchar
+ *
+ * Return: 0 success
+*/
+
+int main(void)
+{
+	_putchar('_putchar');
+	_putchar('\n');
+
+	return (0);
+}
