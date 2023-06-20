a1cf41742cb824f06222cbaeb32435b35bfdc3a8
Athar Hasan <athar_h12@yahoo.com>
2023-06-20 09:09:22 -0700

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,8 +10,11 @@
 
 int main(void)
 {
-	_putchar('_putchar');
-	_putchar('\n');
+	char str[] = '_putchar';
+	int i;
 
+	for (i = 0; i <= 7; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 	return (0);
 }
