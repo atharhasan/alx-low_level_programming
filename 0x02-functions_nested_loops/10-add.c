0ae5e0282df8d5575a8b493f8b54db1e1b9996f1
Athar Hasan <athar_h12@yahoo.com>
2023-06-20 16:04:47 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * add - function
+ *
+ * Description: function that can add two int and return result
+ *
+ * @a: integer param
+ * @b: integer param
+ *
+ * Return: result of add two int
+*/
+
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+
+	_putchar(res + '0');
+	return (res);
+}
