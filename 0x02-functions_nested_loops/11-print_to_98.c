2ef8a6b2102243256da8a6ec38126901f75c1119
Athar Hasan <athar_h12@yahoo.com>
2023-06-20 16:28:30 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_to_89 - function
+ *
+ * Description: function that can print numbers to 89
+ *
+ * @n: int param
+*/
+
+void print_to_89(int n)
+{
+	while (n < 89)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+}
