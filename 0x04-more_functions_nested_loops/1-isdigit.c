a275e880ec9d8d52cd10cd9ef05e0f5f2eafa1d7
Athar Hasan <athar_h12@yahoo.com>
2023-06-21 21:03:07 -0700

    more loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,20 +2,19 @@
 #include <ctype.h>
 
 /**
- * _isupper - function
+ * _isdigit - function
  *
- * Description: function that can check uppercase char
+ * Description: function that checks for a digit
  *
  * @c: input param
  *
  * Return: 1 for true 0 for false
 */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (isupper(c))
+	if (isdigit(c))
 		return (1);
 	else
 		return (0);
-
 }
