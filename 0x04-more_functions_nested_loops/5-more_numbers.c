c27814017fec94fe1c00b534fb135b4c364f1207
Athar Hasan <athar_h12@yahoo.com>
2023-06-22 15:41:03 -0700

    more loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+/**
+ * more_numbers- function
+ *
+ * Description: function that can print numbers from 0 to 14 - 10 times
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+		for (j = 0; j < 15; j++)
+		{
+			putchar(j);
+		}
+	}
+}
