04793c5592b9c9396ef5b1101a9a0ac9dfe89a35
Athar Hasan <athar_h12@yahoo.com>
2023-06-22 16:24:34 -0700

    more loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - function
+ *
+ * Description: function that can print _ char
+ *
+ * @n: input of _ count
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n >= 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	} else
+	{
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
