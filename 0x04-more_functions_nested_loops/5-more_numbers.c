7052e7261b24834ea52b70dd8f88af827304ac0c
Athar Hasan <athar_h12@yahoo.com>
2023-06-22 16:01:55 -0700

    more loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,20 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, num;
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar(i + '0');
-		_putchar('\n');
 		for (j = 0; j < 15; j++)
 		{
-			_putchar(j + '0');
+			num = j;
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				num = j % 10;
+			}
+			_putchar(num + 48);
 		}
+		putchar('\n');
 	}
 }
