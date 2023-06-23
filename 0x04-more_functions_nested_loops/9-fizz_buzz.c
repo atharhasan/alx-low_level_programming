23bc7a4fc729aa17e9e6e7f5be6af475bf36b631
Athar Hasan <athar_h12@yahoo.com>
2023-06-22 19:04:56 -0700

    more loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,15 +18,12 @@ int main(void)
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
-			continue;
 		} else if (i % 3 == 0)
 		{
 			printf("Fizz");
-			continue;
 		} else if (i % 5 == 0)
 		{
 			printf("Buzz");
-			continue;
 		} else
 			printf("%d", i);
 		if (i != 100)
