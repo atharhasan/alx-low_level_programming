ce8c2bd2e0d4a24b6fb919a8d365523507374239
Athar Hasan <athar_h12@yahoo.com>
2023-06-26 03:45:15 -0700

    commit task

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,12 +15,12 @@ void puts_half(char *str)
 	len = strlen(str);
 	for (i = len / 2; i < len; i++)
 	{
-		/**
-		 * if (len % 2 != 0)
-		 * _putchar(str[i + 1]);
-		 * else
-		 */
-		_putchar(str[i + 1]);
+		if (i % 2 != 0)
+		{
+			i = len - 1 / 2;
+			_putchar(str[i]);
+		} else
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
