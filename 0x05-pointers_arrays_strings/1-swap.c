62cc3f93255e2740be0b6ca64236d516cb4a9e9e
Athar Hasan <athar_h12@yahoo.com>
2023-06-25 21:51:38 -0700

    commit task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - function
+ *
+ * Description: function that swaps the values of two integers
+ *
+ * @a: input
+ *
+ * @b: input
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
