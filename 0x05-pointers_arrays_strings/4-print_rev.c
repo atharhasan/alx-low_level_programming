e8ec1b58807d03e039058c0ab4038f7868bca311
Athar Hasan <athar_h12@yahoo.com>
2023-06-25 23:16:44 -0700

    commit task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - function
+ *
+ * Description: prints a string, in reverse, followed by a new line.
+ *
+ * @s: input
+*/
+
+void print_rev(char *s)
+{
+	strrev(s);
+}
