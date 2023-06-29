4238bcde1789a6bf8843b3e7e3e0dab9cfd53845
Athar Hasan <athar_h12@yahoo.com>
2023-06-28 23:24:35 -0700

    commit task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - function
+ *
+ * Description: function that concatenates two strings
+ *
+ * @dest: input
+ * @src: input
+ * @n: input
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+}
