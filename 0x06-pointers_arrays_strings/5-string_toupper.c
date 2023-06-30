6f414677531b86e1d8f5a76dc4a7838d77422815
Athar Hasan <athar_h12@yahoo.com>
2023-06-29 18:39:40 -0700

    commit task

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,8 +13,14 @@
 
 char *string_toupper(char *str)
 {
-	char ch;
+	int i;
 
-	ch = toupper(str);
-	return (ch);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
 }
