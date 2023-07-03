e73112db8dcf632643180278f6b28e7fb4557bf0
Athar Hasan <athar_h12@yahoo.com>
2023-07-03 02:09:57 -0700

    commit task

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,12 +13,12 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i < sizeof(a); i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			printf("%c", a[i][j]);
+			_putchar('a[i][j]');
 		}
-		printf("\n");
+		_putchar('\n');
 	}
 }
