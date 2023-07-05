b4f57d86375bc2fe954f847ee6641757f37c0198
Athar Hasan <athar_h12@yahoo.com>
2023-07-05 01:28:55 -0700

    commit task

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -20,7 +20,13 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 	{
 		return (-1);
+	} else if (y == 0)
+	{
+		return (1);
+	} else if (y == 1)
+	{
+		return (x);
 	}
-	res = pow(x, y);
+	res = x * _pow_recursion(x, y - 1);
 	return (res);
 }
