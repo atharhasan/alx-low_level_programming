8af60a44a07aedb15643bbd0c772a552a1e058cd
Athar Hasan <athar_h12@yahoo.com>
2023-07-05 00:28:21 -0700

    commit task

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - function
+ *
+ * Description:function that returns the length of a string
+ *
+ * @s: input
+ *
+ * Return: the length of a string
+*/
+
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
