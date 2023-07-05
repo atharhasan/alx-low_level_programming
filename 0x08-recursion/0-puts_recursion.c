d2a0ebb19a88ae4d907b49e197c408d67cee8b99
Athar Hasan <athar_h12@yahoo.com>
2023-07-04 23:44:44 -0700

    commit task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts_recursion - function
+ *
+ * Description:function that prints a string
+ *
+ * @s: input
+*/
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
