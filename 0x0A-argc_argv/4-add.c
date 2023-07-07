63c8269e5f42916cbc3e5750bb69d242caff1652
Athar Hasan <athar_h12@yahoo.com>
2023-07-07 05:46:23 -0700

    commit task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,19 +17,22 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	for (i = 1; i < argc; i++)
+	if (argc == 1)
 	{
-		if (isdigit(argv[i]))
+		printf("0\n");
+	} else
+	{
+		for (i = 1; i < argc; i++)
 		{
-			sum += atoi(argv[i]);
-			printf("%d\n", sum);
-		} else if (argc == 0)
-		{
-			printf("0\n");
-		} else
-		{
-			printf("Error\n");
-			return (1);
+			if (isdigit(argv[i]))
+			{
+				sum += atoi(argv[i]);
+				printf("%d\n", sum);
+			} else
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 	}
 	return (0);
