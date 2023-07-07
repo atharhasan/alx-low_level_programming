7d4c224dafd3be7f75d2141962c4976b552a7c42
Athar Hasan <athar_h12@yahoo.com>
2023-07-07 04:32:39 -0700

    commit task

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Description: write a program that print its name
+ *
+ * @argc: input
+ * @argv: input
+ *
+ * Return: 0 always
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc]);
+	return (0);
+}
