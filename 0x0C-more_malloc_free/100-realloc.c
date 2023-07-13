24ba59ea45752fd52aca0d2fa1fc2d9a87265c90
Athar Hasan <athar_h12@yahoo.com>
2023-07-13 16:08:21 -0700

    commit task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -39,7 +39,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		k = realloc(ptr, new_size);
 		if (k == NULL)
 			return (NULL);
-		free(ptr);
 	}
 	return (k);
+	free(ptr);
 }
