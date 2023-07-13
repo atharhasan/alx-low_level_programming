985bcc15cb1164b3b6865729e25f8477b54ab780
Athar Hasan <athar_h12@yahoo.com>
2023-07-13 10:32:23 -0700

    commit task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,9 +27,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	size1 = strlen(s1);
 	size2 = strlen(s2);
-	c = malloc((size1 + size2) * sizeof(char) + 1);
+	c = malloc((size1 + n) * sizeof(char) + 1);
 	if (c == NULL)
-		return (0);
+		return (NULL);
 	for (i = 0; i < size1; i++)
 	{
 		c[i] = s1[i];
