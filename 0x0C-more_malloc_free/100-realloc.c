31bf5df34666888d414bb980baa635735207cf40
Athar Hasan <athar_h12@yahoo.com>
2023-07-13 15:58:47 -0700

    commit task

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - nested loop to make grid
+ *
+ * Descreption: function that reallocates a memory block using malloc and free
+ *
+ * @ptr: the pointer of array
+ * @old_size: old size of array
+ * @new_size: new size of array
+ *
+ * Return: ptr
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *k;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		k = realloc(ptr, new_size);
+		if (k == NULL)
+			return (NULL);
+		return (k);
+	}
+	if (new_size == 0 && ptr != 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		k = realloc(ptr, new_size);
+		if (k == NULL)
+			return (NULL);
+		free(ptr);
+		return (k);
+	}
+}
