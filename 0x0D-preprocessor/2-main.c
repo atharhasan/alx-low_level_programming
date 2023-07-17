dd38d96cbe2e373573b4bd5b9ec76eec894d29ea
Athar Hasan <athar_h12@yahoo.com>
2023-07-17 15:42:12 -0700

    commit task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Descreption: function thar print the name of the file
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
