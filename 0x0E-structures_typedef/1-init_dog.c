85c47e9ce9ff62b3654ac7700e5db0fe81f45a3d
Athar Hasan <athar_h12@yahoo.com>
2023-07-17 20:11:07 -0700

    commit task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ *
+ * @name: a dog name
+ * @age: a dog age
+ * @owner: a dog owner's
+ *
+ * Description: struct to define this arg
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
