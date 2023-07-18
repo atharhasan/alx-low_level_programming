2ea976aab553ea6552d601b8997609d76a917a8a
Athar Hasan <athar_h12@yahoo.com>
2023-07-17 20:01:33 -0700

    commit task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ *
+ * @name: a dog name
+ * @age: a dog age
+ * @owner: a dog owner's
+ *
+ * Description: struct to define this arg
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
