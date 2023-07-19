82ffa46d734d7aeb852ce87245f74697163dff92
Athar Hasan <athar_h12@yahoo.com>
2023-07-19 06:39:16 -0700

    commit task

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function
+ *
+ * Descreption:function that searches for an integer.
+ *
+ * @array: input
+ * @size: input
+ * @cmp: input
+ *
+ * Return: he index of the first element
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && size && cmp)
+	{
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
+		return (-1);
+	}
+}
