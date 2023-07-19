a21dc956f5ee21dde0a7b81c8d4159ac2702b862
Athar Hasan <athar_h12@yahoo.com>
2023-07-19 16:20:22 -0700

    commit task

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,81 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function
+ *
+ * Description: function that sum 2 integar
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: the sum of a and b
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function
+ *
+ * Description: function that return the difference of 2 integar
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: the difference of a and b
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function
+ *
+ * Description: function that return the product of 2 integar
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: the product of a and b
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function
+ *
+ * Description: function that return the division of 2 integar
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: the division of a and b
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function
+ *
+ * Description: function that return the remainder of 2 integar
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: the remainder of a and b
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
