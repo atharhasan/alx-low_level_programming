7fd73b57d51eeb5b36bfc1c874a96df73b8e3b88
Athar Hasan <athar_h12@yahoo.com>
2023-07-20 12:04:16 -0700

    commit task

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <string.h>
+
+/**
+ * print_strings - function that prints strings followed by new line
+ *
+ * @separator: char input
+ * @n: unsigned num
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list li;
+	unsigned int i;
+
+	if (!n)
+	{
+		printf("nil\n");
+		return;
+	}
+
+	va_start(li, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(li, unsigned int));
+
+		if (i < n - 1 && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(li);
+}
