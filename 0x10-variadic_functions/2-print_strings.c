41f744836f562d81a99f0586c6a7e1d2ff9ba710
Athar Hasan <athar_h12@yahoo.com>
2023-07-20 15:09:17 -0700

    commit task

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("nil");
 		printf("%s", ch);
 
-		if (i < n - 1 && separator)
+		if (i != n && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
