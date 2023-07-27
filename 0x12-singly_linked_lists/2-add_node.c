9be49495073c54b8678ec16d6abf4be3e14ecb83
atharhasan <athar_h12@yahoo.com>
2023-07-27 13:00:06 -0700

    commit task

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,16 +11,21 @@ i#include "lists.h"
 list_t *add_node(list_t **head, const char *str)
 {
 	char *c = strdup(str);
-	list_t *new_node;
+	list_t *new_node = malloc(sizeof(list_t));
 
-	if (c)
+	if (!head || !new_node)
+		return (NULL);
+
+	if (str)
 	{
-		while (head != NULL)
+		new_node->str = c;
+		if (!new_node->str)
 		{
-			new_node->str = c;
-			new_node->next = *head;
-			*head = new_node;
+			free(new_node);
+			return (NULL);
 		}
 	}
+	new_node->next = *head;
+	*head = new_node;
 	return (new_node);
 }
