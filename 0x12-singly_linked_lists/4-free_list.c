d113c22772ab040e518e84ef397a838d92be3286
atharhasan <athar_h12@yahoo.com>
2023-07-27 15:03:26 -0700

    commit task

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,29 +7,6 @@
 
 void free_list(list_t *head)
 {
-	char *c = strdup(str);
-	list_t *new_node = malloc(sizeof(list_t));
-	list_t *node = *head;
-
-	if (!head || !new_node)
-		return (NULL);
-
-	if (str)
-	{
-		new_node->str = c;
-		if (!new_node->str)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		new_node->len = strlen(new_node->str);
-	}
-	if (node)
-	{
-		while (node->next)
-			node = node->next;
-		node->next = new_node;
-	} else
-		*head = new_node;
-	free(head);
+	if (!head)
+		free(head);
 }
