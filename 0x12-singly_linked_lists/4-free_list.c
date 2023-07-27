1e9c712aa4a701f2f7833f9f9fcb5bba08af8d0f
atharhasan <athar_h12@yahoo.com>
2023-07-27 15:11:38 -0700

    commit task

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,6 +7,17 @@
 
 void free_list(list_t *head)
 {
+	list_t *node, *next_node;
+
 	if (!head)
-		free(head);
+		return;
+	node = head;
+
+	while (node)
+	{
+		next_node = node->next;
+		free(node->str);
+		free(node);
+		node = next_node;
+	}
 }
