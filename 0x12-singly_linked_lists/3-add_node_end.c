276b7a2302a4ceff02fa6e80100a3257b2edfcf5
atharhasan <athar_h12@yahoo.com>
2023-07-27 14:42:11 -0700

    commit task

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_node - function that can add new node
+ * @head: pointer from struct
+ * @str: the value of new node
+ *
+ * Return: the address of new element
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *c = strdup(str);
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (!head || !new_node)
+		return (NULL);
+
+	if (str)
+	{
+		new_node->str = c;
+		if (!new_node->str)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->len = strlen(new_node->str);
+	}
+	new_node->next = NULL;
+	return (new_node);
+}
