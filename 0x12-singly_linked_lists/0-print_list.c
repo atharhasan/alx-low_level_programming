0e7c28d2f512f2980feca0cda9feaaf668e22ee0
atharhasan <athar_h12@yahoo.com>
2023-07-27 11:00:15 -0700

    commit task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,9 +13,7 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)");
-		printf("[%d] %s\n", h->len, h->str);
+		printf("[%d] %s\n", h->len, h->str != NULL ? h->str : "(nil)");
 		h = h->next;
 		count++;
 	}
