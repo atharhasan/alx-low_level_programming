71416a801d35c845e76c86e0f4e4aab675241481
atharhasan <athar_h12@yahoo.com>
2023-07-31 23:23:59 +0300

    commit task

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that can add new node
+ * @head: pointer from struct
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *node, *next_node;
+
+	if (!head)
+		return;
+	node = head;
+
+	while (node)
+	{
+		next_node = node->next;
+		free(node);
+		node = next_node;
+	}
+}
