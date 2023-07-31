06e314bcd46b7c38456b20eb98232f45617c0193
atharhasan <athar_h12@yahoo.com>
2023-08-01 00:28:12 +0300

    commit task

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listin - function that can add new node
+ * @head: pointer from struct
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int n;
+
+	if (!head || !*head)
+		return (0);
+
+	node = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = node;
+	return (n);
+}
