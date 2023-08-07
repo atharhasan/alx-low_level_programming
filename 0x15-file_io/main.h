28ce480fb6864fa50d8654a5f9816d7a910526e3
atharhasan <athar_h12@yahoo.com>
2023-08-08 01:25:25 +0300

    commit task

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
