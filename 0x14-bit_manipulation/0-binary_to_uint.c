508ee8a9b05ff5b5091745b7ef6eb9d11454308d
atharhasan <athar_h12@yahoo.com>
2023-10-27 00:28:11 +0200

    0-0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,16 +15,15 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int res;
 	int i;
-	if (b == NULL)
-		return(0);
 
-	for (i=0; b[i] < '\0'; i++)
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '1' && b[i] != '0')
 			return (0);
-			
 	res = (res << 1) | (b[i] - '0');
 	}
-
-	return (res);
+return (res);
 }
