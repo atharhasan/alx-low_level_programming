081d40ef04947d19a9ced087e3bba3a2bcd3ba2c
atharhasan <athar_h12@yahoo.com>
2023-10-31 01:58:34 +0200

    :(

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - that creates a file
+ * @filename: the postion that will returned value
+ * @letters: the value
+ *
+ * Return: 0
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int FOpen;
+	ssize_t byteSize;
+
+	FOpen = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (FOpen == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		byteSize = write(FOpen, text_content, strlen(text_content));
+		if (byteSize == -1)
+		{
+			close(FOpen);
+			return (-1);
+		}
+		close(FOpen);
+	}
+	close(FOpen);
+	return (1);
+}
