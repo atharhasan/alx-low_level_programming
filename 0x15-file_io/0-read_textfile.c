7b1296d5c25152299525f13c02c407884f11e6e1
atharhasan <athar_h12@yahoo.com>
2023-10-31 01:30:45 +0200

    first commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * read_textfile -function that reads a text file and prints it to the POSIX standard output
+ * @filename: the postion that will returned value
+ * @letters: the value
+ *
+ * Return: 0
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	int pf;
+	ssize_t bytes;
+
+	pf = open(filename, O_RDONLY);
+
+	if (pf == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	bytes = read(pf, buf, letters);
+	bytes = write(STDOUT_FILENO, buf, bytes);
+
+	free(buf);
+	close(pf);
+
+	return (bytes);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+Readme file for FILE IO
+
