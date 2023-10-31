2bef396a24f77ca367e23e80937c63dac65a47ed
atharhasan <athar_h12@yahoo.com>
2023-10-31 04:32:43 +0200

    :(

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -78,7 +78,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO,"%s\n", "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
