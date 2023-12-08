13e3d37f9093ca169196eae9afcd6062feac310e
atharhasan <athar_h12@yahoo.com>
2023-12-08 02:26:21 +0200

    commit task

--- 0x17-doubly_linked_lists/0-main.c
+++ 0x17-doubly_linked_lists/0-main.c
@@ -3,30 +3,6 @@
 #include <stdio.h>
 #include "lists.h"
 
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
-{
-	dlistint_t *temp, *ptr;
-	temp = malloc(sizeof(dlistint_t));
-
-	if (temp == NULL)
-		return (NULL);
-	temp->n = n;
-	temp->next = NULL;
-	if (*head == NULL)
-	{
-		temp->prev = NULL;
-		*head = temp;
-	}
-	else
-	{
-		ptr = *head;
-		while (ptr->next != NULL)
-			ptr = ptr->next;
-		ptr->next = temp;
-		temp->prev = ptr;
-	}
-	return (*head);
-}
 
 size_t print_dlistint(const dlistint_t *h)
 {
--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,5 +7,10 @@
 
 void free_dlistint(dlistint_t *head)
 {
-
+	if (head == NULL)
+		return (NULL);
+	if (head->next == NULL)
+	{
+		free(head);
+	}
 }
