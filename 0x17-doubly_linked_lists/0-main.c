4fa5db27fde0d2b81a297fe8b2954a74fabee942
atharhasan <athar_h12@yahoo.com>
2023-12-08 03:08:10 +0200

    commit task

--- 0x17-doubly_linked_lists/0-main.c
+++ 0x17-doubly_linked_lists/0-main.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include "lists.h"
 
-
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *ptr;
@@ -19,6 +18,58 @@ size_t print_dlistint(const dlistint_t *h)
 	return (num);
 }
 
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *temp, *ptr;
+
+	temp = malloc(sizeof(dlistint_t));
+
+	if (temp == NULL)
+		return (NULL);
+	temp->n = n;
+	temp->next = NULL;
+	if (*head == NULL)
+	{
+		temp->prev = NULL;
+		*head = temp;
+	}
+	else
+	{
+		ptr = *head;
+		while (ptr->next != NULL)
+			ptr = ptr->next;
+		ptr->next = temp;
+		temp->prev = ptr;
+	}
+	return (*head);
+}
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
+}
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *ptr = head;
+
+	if (head == NULL)
+		return (NULL);
+	while (ptr != NULL && index > 0)
+	{
+		ptr = ptr->next;
+		index--;
+	}
+	return (ptr);
+}
+
 /**
  * main - check the code
  *
@@ -27,6 +78,7 @@ size_t print_dlistint(const dlistint_t *h)
 int main(void)
 {
 	dlistint_t *head;
+	dlistint_t *node;
 
 	head = NULL;
 	add_dnodeint_end(&head, 0);
@@ -38,5 +90,9 @@ int main(void)
 	add_dnodeint_end(&head, 402);
 	add_dnodeint_end(&head, 1024);
 	print_dlistint(head);
+	node = get_dnodeint_at_index(head, 5);
+	printf("%d\n", node->n);
+	free_dlistint(head);
+	head = NULL;
 	return (EXIT_SUCCESS);
 }
--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - function returns the nth node of lists
+ * get_dnodeint_at_index - function returns the nth node of lists
  * @head: struct linked list
  * @index: is the index of the node that should be deleted
  *
@@ -10,5 +10,14 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	dlistint_t *ptr = head;
 
+	if (head == NULL)
+		return (NULL);
+	while (ptr != NULL && index > 0)
+	{
+		ptr = ptr->next;
+		index--;
+	}
+	return (ptr);
 }
