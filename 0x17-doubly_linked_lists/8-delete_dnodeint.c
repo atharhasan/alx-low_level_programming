8557cb8047fe8819cac23b6051316939cd16e0b3
atharhasan <athar_h12@yahoo.com>
2023-12-08 03:34:57 +0200

    commit task

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -6,8 +6,42 @@
  * @index: is the index of the node that should be deleted
  *
  * Return: 1 if it succeeded, -1 if it failed
-*/
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	
+	dlistint_t *current = *head;
+	dlistint_t *temp;
+	unsigned int count = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(temp);
+		return (1);
+	}
+
+	while (current != NULL && count < index)
+	{
+		current = current->next;
+		count++;
+	}
+
+	if (current == NULL)
+		return (-1);
+
+	temp = current;
+	current->prev->next = current->next;
+
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+
+	free(temp);
+
+	return (1);
 }
