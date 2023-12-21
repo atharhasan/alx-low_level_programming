5c1de4ad799b6a483f2c34b1e5f952fade65face
atharhasan <athar_h12@yahoo.com>
2023-12-21 10:11:41 +0200

    commit task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
-	if (table == NULL || size == NULL)
+	if (table == NULL || size <= 0)
 		return (NULL);
 
 	table->size = size;
