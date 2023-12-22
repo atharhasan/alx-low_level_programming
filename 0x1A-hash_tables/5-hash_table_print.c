05f938a4cb6a47460cf69c0fbe382d70d32ebf0c
atharhasan <athar_h12@yahoo.com>
2023-12-22 09:27:37 +0200

    commit task

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - function that prints a hash table.
+ * @ht: is the hash table you want to add or update the key/val
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *ptr;
+	int flag = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+
+	for(i = 0; i < ht->size; i++)
+	{
+		ptr = ht->array[i];
+		while (ptr != NULL)
+		{
+			if(flag)
+				printf(", ");
+			printf("'%s': '%s'", ptr->key, ptr->value);
+			flag = 1;
+			ptr = ptr->next;
+		}
+	}
+	printf("}\n");
+}
