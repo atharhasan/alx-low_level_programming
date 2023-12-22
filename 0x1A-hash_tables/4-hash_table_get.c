eeae8c11a6b2e0a42e70eeff459a1e01aff8bf61
atharhasan <athar_h12@yahoo.com>
2023-12-22 08:55:28 +0200

    commit task

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - function retrieves a value associated with a key.
+ * @ht: is the hash table you want to add or update the key/val
+ * @key: is the key.
+ * Return: the value element, or NULL if key couldn’t be found
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *value;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index(key, ht->size);
+	value = ht->array[index];
+
+	while (value != NULL)
+	{
+		if (strcmp(value->key, key) == 0)
+			return (value->value);
+		value = value->next;
+	}
+	return (NULL);
+}
